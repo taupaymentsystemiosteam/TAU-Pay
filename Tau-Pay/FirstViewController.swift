e228aaa7820cdf72a9f647767ee47e944d3d6830
Nusret Özateş <nusretozates@Nusrets-MacBook-Air.local>
2019-03-16 15:54:35 +0300

    Initial Commit

---
+++ Tau-Pay/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tau-Pay
+//
+//  Created by Nusret Özateş on 16.03.2019.
+//  Copyright © 2019 Nusret Özateş. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tau-Pay/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tau-Pay
+//
+//  Created by Nusret Özateş on 16.03.2019.
+//  Copyright © 2019 Nusret Özateş. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
