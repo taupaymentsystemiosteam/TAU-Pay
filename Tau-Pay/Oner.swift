fec6f9c5b2b5559735637844d1822d05980a3977
Nusret Özateş <nusretozates@Nusrets-MacBook-Air.local>
2019-03-16 17:05:06 +0300

    Added all project things

---
+++ Tau-Pay/Oner.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  Tau-Pay
+//
+//  Created by Nusret Özateş on 16.03.2019.
+//  Copyright © 2019 Nusret Özateş. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
