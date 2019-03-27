6bd1a09a0b213974cf3a14ac77e7a54af3349cc8
NusretOzates <nozatespc@gmail.com>
2019-03-27 22:28:26 +0300

    Para yollama sayfasi halledildi.

--- Tau-Pay/ParaGonder.swift
+++ Tau-Pay/ParaGonder.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ParaGonder: UIViewController {
 
+    @IBOutlet weak var moneyBetrag: UITextField!
+    @IBOutlet weak var studentNumber: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +21,35 @@ class ParaGonder: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func SendMoney(_ sender: Any)
+    {
+        /*
+            Ilk olarak ogrenci numarasi servera gidip kim oldugu ogrenilecek
+            Sonra bi emin misiniz yazisi cikacak
+            Eminse para gonderilecek.
+         */
+        let name = "Nusret"
+        let alert = UIAlertController(title: "Emin misin", message: "\(name) adli ogrenciye para gonderilecek emin misiniz ?", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "Eminim", style: UIAlertAction.Style.default, handler: {(action) in
+            self.sendMoney()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Iptal", style: UIAlertAction.Style.default, handler: {(action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
     }
-    */
+    
+    func sendMoney() {
+        
+    }
+    
+    
+    
+    
+    
 
 }
