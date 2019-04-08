02b87ca5ac9db210a366059df3ae53b94760b186
Beyza <beyza786@outlook.com>
2019-04-08 14:53:21 +0300

    star functions

--- Tau-Pay/FeedbackViewController.swift
+++ Tau-Pay/FeedbackViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class FeedbackViewController: UIViewController {
 
+    @IBOutlet var Star1: UIButton!
+    @IBOutlet var Star2: UIButton!
+    @IBOutlet var Star3: UIButton!
+    @IBOutlet var Star4: UIButton!
+    @IBOutlet var Star5: UIButton!
+    
+    
+    @IBAction func Star1Action(_ sender: Any) {
+    }
+    
+    @IBAction func StarAction2(_ sender: Any) {
+        Star1.setImage(UIImage(named: "circled_pay"), for: <#T##UIControl.State#>)
+    }
+    @IBAction func StarAction3(_ sender: Any) {
+    }
+    @IBAction func StarAction4(_ sender: Any) {
+    }
+    @IBAction func StarAction5(_ sender: Any) {
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
                 
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
     }
     
 
