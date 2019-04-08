2cfdde407e45a7a0d8d4122d10bb3ea0a46f34f8
Beyza <beyza786@outlook.com>
2019-04-08 20:02:33 +0300

    Feedback

--- Tau-Pay/FeedbackViewController.swift
+++ Tau-Pay/FeedbackViewController.swift
@@ -10,27 +10,91 @@ import UIKit
 
 class FeedbackViewController: UIViewController {
 
+    @IBOutlet var AcıklamaText: UITextView!
     @IBOutlet var Star1: UIButton!
     @IBOutlet var Star2: UIButton!
     @IBOutlet var Star3: UIButton!
     @IBOutlet var Star4: UIButton!
     @IBOutlet var Star5: UIButton!
+    @IBOutlet var MensaShutteSelect: UISegmentedControl!
+    @IBOutlet var GonderButton: UIButton!
     
+    @IBOutlet var YorumText: UITextView!
     
 
-    @IBAction func Star1Action(_ sender: Any) {
+    @IBAction func StarAction1(_ sender: Any) {
+        
+        //Filling 1 Star
+        Star1.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        
+        //Emptying 4 Stars
+        Star2.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+        Star3.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+        Star4.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+        Star5.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
     }
     
     @IBAction func StarAction2(_ sender: Any) {
-        Star1.setImage(UIImage(named: "circled_play"), for: UIControl.State.normal)
-        Star2.setImage(UIImage(named: "circled_play"), for: UIControl.State.normal)
+        //Filling 2 Stars
+        Star1.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star2.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        
+        //Emptying 3 Stars
+        Star3.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+        Star4.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+        Star5.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+
     }
     @IBAction func StarAction3(_ sender: Any) {
+        
+        //Filling 3 Stars
+        Star1.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star2.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star3.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        
+        //Emptying 2 Stars
+        Star4.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+        Star5.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+        
+        
     }
     @IBAction func StarAction4(_ sender: Any) {
+        
+        //Filling 4 Stars
+        Star1.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star2.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star3.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star4.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        
+        //Emptying 1 Star
+        Star5.setImage(UIImage(named: "empty_star"), for: UIControl.State.normal)
+
+        
     }
     @IBAction func StarAction5(_ sender: Any) {
+        
+        //Filling 5 Stars
+        
+        Star1.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star2.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star3.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star4.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+        Star5.setImage(UIImage(named: "filled_star"), for: UIControl.State.normal)
+
     }
+    
+    
+    @IBAction func MensaShuttleSelect(_ sender: Any) {
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func GonderButton(_ sender: Any) {
+    }
+    
     
     
     
