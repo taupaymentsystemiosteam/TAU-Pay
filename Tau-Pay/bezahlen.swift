0eb9d6468993bac1331c71e41c501b8ab89b5e54
Sevcan Avci <sevcan_avci@outlook.de>
2019-04-10 00:11:30 +0300

    bezahlen updates + login (alert :"try again")

---
+++ Tau-Pay/bezahlen.swift
@@ -0,0 +1,24 @@
+//
+//  bezahlen.swift
+//  Tau-Pay
+//
+//  Created by Sevcan Avcı on 09.04.19.
+//  Copyright © 2019 Nusret Özateş. All rights reserved.
+//
+
+import UIKit
+
+class BezahlenViewController: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+//    @IBAction func odebutton(_ sender: Any) {
+//        
+//        
+//    }
+}
