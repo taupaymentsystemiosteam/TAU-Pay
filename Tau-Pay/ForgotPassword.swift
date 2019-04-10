c4846e305a358dbaef667b5b3bfc2b75d62472ab
NusretOzates <nozatespc@gmail.com>
2019-04-10 16:35:24 +0300

    Added feature -Forgot password page

---
+++ Tau-Pay/ForgotPassword.swift
@@ -0,0 +1,42 @@
+//
+//  ForgotPassword.swift
+//  Tau-Pay
+//
+//  Created by Nusret Özateş on 10.04.2019.
+//  Copyright © 2019 Nusret Özateş. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPassword: UIViewController {
+
+    @IBAction func sendButton(_ sender: Any) {
+        
+        //let userNumber =
+        
+        
+    }
+    @IBAction func cancelButton(_ sender: Any) {
+        
+        
+        
+    }
+    @IBOutlet weak var numberBox: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
