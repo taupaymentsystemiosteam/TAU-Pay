f02b53e635c71c74667ede30e0cf252d59fc80f3
NusretOzates <nozatespc@gmail.com>
2019-04-10 21:36:46 +0300

    Generification - Generification of the change password code.  All of the popup creation is now done from a singular function.

--- Tau-Pay/ChangePasswordViewController.swift
+++ Tau-Pay/ChangePasswordViewController.swift
@@ -9,46 +9,60 @@
 import UIKit
 
 class ChangePasswordViewController: UIViewController {
-
+    
     @IBOutlet weak var newPassRepeated: UITextField!
     @IBOutlet weak var newPass: UITextField!
+    @IBOutlet weak var oldPass: UITextField!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    func createAnimatedPopUp(title: String, message: String) {
+        let alert =  UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: {(action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alert, animated: true, completion: nil)
+        return
+    }
+    
+    
     @IBAction func ChangePassword(_ sender: Any)
     {
+        if oldPass.text == "" || newPass.text == "" || newPassRepeated.text == "" {
+            createAnimatedPopUp(title: "Hata", message: "Kutuların içi boş olamaz")
+            return
+        }
+        
         if newPass.text == newPassRepeated.text {
             let json = ["newPass":newPass.text]
             let response = Constants.SendRequestGetString(requestType: "/customers/change-password", json: json as Dictionary<String, Any>)
-           
-            if let responseInfo = response.info
-           {
-            let alert = UIAlertController(title: "Result", message: "\(String(describing: responseInfo))", preferredStyle: UIAlertController.Style.alert)
             
-            alert.addAction(UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: {(action) in
-                alert.dismiss(animated: true, completion: nil)
-            }))
-            self.present(alert,animated: true,completion: nil)
+            
+            if response.connectionError {
+                // Handle connection error
+                createAnimatedPopUp(title: "Hata", message: "Bağlantı hatası, internete bağlantınızı kontrol ediniz ve birazdan tekrar deneyiniz")
+                return
             }
-        
-        }else
-        {
-            let alert = UIAlertController(title: "Result", message: "Şifreler uyuşmuyor AQ.", preferredStyle: UIAlertController.Style.alert)
+            if response.error != nil {
+                // Handle improper connection
+                createAnimatedPopUp(title: "Hata", message: "Hatalı giriş")
+                return
+            }
             
-            alert.addAction(UIAlertAction(title: "Tamam AQ", style: UIAlertAction.Style.default, handler: {(action) in
-                alert.dismiss(animated: true, completion: nil)
-            }))
-            self.present(alert,animated: true,completion: nil)
+            if let responseInfo = response.info {
+                createAnimatedPopUp(title: "Sonuç", message: "Para başarıyla gönderildi. \(String(describing: response.info!))")
+            }
+            
+        } else {
+            createAnimatedPopUp(title: "Sonuç", message: "Şifreler uyuşmuyor AQ.")
         }
-        
-        
     }
     
 }
