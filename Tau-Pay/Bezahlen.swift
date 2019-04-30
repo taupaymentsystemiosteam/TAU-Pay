5ba0d9fd7728c5ccf8e8d26001960a1fbdc0efbc
tau <tau@tau-mac-mini-4.local>
2019-04-30 14:28:51 +0300

    qrcode function

--- Tau-Pay/Bezahlen.swift
+++ Tau-Pay/Bezahlen.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class Bezahlen: UIViewController {
     
-    @IBOutlet weak var progressBar: UIProgressView!
+    static var qrString = ""
     
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var qrCodeImage: UIImageView!
+    var progressValue = 1.0
+    
+    static func setString(qr: String){
+        qrString = qr
+    }
     
     func createAnimatedPopUp(title: String, message: String) {
         let alert =  UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
@@ -32,7 +38,6 @@ class Bezahlen: UIViewController {
         let response = Constants.SendRequestGetString(requestType: "/customers/request-qr-code", json: dict)
     
     
-        
         if response.connectionError {
             // Handle connection error
             createAnimatedPopUp(title: "Hata", message: "Bağlantı hatası, internete bağlantınızı kontrol ediniz ve birazdan tekrar deneyeniz")
@@ -45,17 +50,40 @@ class Bezahlen: UIViewController {
             return
         }
         
-        QrCodeController.setString(qr: response.info!)
+        let data = Bezahlen.qrString.data(using:String.Encoding.ascii)
+        
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 13, y: 13)
+            //print("bob")
+            if let output = filter.outputImage?.transformed(by: transform) {
+                qrCodeImage.image = UIImage(ciImage: output)
+            }
+        }
+        self.perform(#selector(updateProgress), with: nil, afterDelay: 0.2)
+        
+        //QrCodeController.setString(qr: response.info!)
         
         
+    }
+    
+    @objc func updateProgress() {
+        progressValue = progressValue - 0.01
+        self.progressBar.progress = Float(progressValue)
+        if progressValue != 0.0 {
+            self.perform(#selector(updateProgress), with: nil, afterDelay: 0.2)
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-    
     }
+    
+    //override func NewviewDidLoad(){
+    //    super.viewDidLoad()
+        
+    //}
     
 
     
