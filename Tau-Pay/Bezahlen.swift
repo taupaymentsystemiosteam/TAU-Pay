2dcd87fc7133b6855faac64bbe0bbab100b3079b
Sevcan Avci <sevcan_avci@outlook.de>
2019-05-04 23:15:05 +0300

    qr code page deleted and bezahlen page changed

--- Tau-Pay/Bezahlen.swift
+++ Tau-Pay/Bezahlen.swift
@@ -12,6 +12,7 @@ class Bezahlen: UIViewController {
     
     static var qrString = ""
     
+    @IBOutlet weak var infotext: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var qrCodeImage: UIImageView!
     var progressValue = 1.0
@@ -32,6 +33,8 @@ class Bezahlen: UIViewController {
     }
     
     @IBAction func paybutton(_ sender: Any) {
+        progressBar.isHidden = false
+        infotext.isHidden = true
         
         let dict: [String: String] = [:]
             
@@ -54,7 +57,7 @@ class Bezahlen: UIViewController {
         
         if let filter = CIFilter(name: "CIQRCodeGenerator") {
             filter.setValue(data, forKey: "inputMessage")
-            let transform = CGAffineTransform(scaleX: 13, y: 13)
+            let transform = CGAffineTransform(scaleX: 17, y: 17)
             //print("bob")
             if let output = filter.outputImage?.transformed(by: transform) {
                 qrCodeImage.image = UIImage(ciImage: output)
@@ -67,16 +70,26 @@ class Bezahlen: UIViewController {
         
     }
     
+    @objc func Ispaid(){
+        
+    }
     @objc func updateProgress() {
         progressValue = progressValue - 0.01
         self.progressBar.progress = Float(progressValue)
         if progressValue != 0.0 {
             self.perform(#selector(updateProgress), with: nil, afterDelay: 0.2)
         }
+        if progressBar.progress == 0    {        infotext.isHidden = false
+            progressBar.isHidden = true
+            qrCodeImage.isHidden = true
+        }
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        progressBar.isHidden = true
+        infotext.text = "Ödeme yapmak için gerekli olan QR kodu oluşturmak için Ödeme tuşuna basınız."
         
     }
     
