ef50c89a08322b4e4c01dc0fc16396c48a6bce0a
NusretOzates <nozatespc@gmail.com>
2019-05-08 13:40:22 +0300

    multithreaded support for recommendation page

--- Tau-Pay/Oner.swift
+++ Tau-Pay/Oner.swift
@@ -30,25 +30,31 @@ class ThirdViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
         
     }
-    func recommendRequest(){
-        let json = ["id": recommendText.text!]
-        
-        let response = Constants.SendRequestGetString(requestType: "/customers/recommend", json: json)
-        if response.connectionError {
-            // Handle connection error
-            createAnimatedPopUp(title: "Hata", message: "Bağlantı hatası, internete bağlantınızı kontrol ediniz ve tekrar deneyiniz.")
-            return
-        }
+    func recommendRequest() {
+        let queue = DispatchQueue(label: "request")
+        queue.async {
+            let json = ["id": self.recommendText.text!]
             
-        else if response.error != nil {
-            // Handle improper connection
-            createAnimatedPopUp(title: "Hata", message: "Hatalı giriş")
-            return
-        } else {
-            createAnimatedPopUp(title: "Başarılı", message: "Öğrenci önerilmiştir.")
+            let response = Constants.SendRequestGetString(requestType: "/customers/recommend", json: json)
+            DispatchQueue.main.async {
+                if response.connectionError {
+                    // Handle connection error
+                    self.createAnimatedPopUp(title: "Hata", message: "Bağlantı hatası, internete bağlantınızı kontrol ediniz ve tekrar deneyiniz.")
+                    return
+                }
+                    
+                else if response.error != nil {
+                    // Handle improper connection
+                    self.createAnimatedPopUp(title: "Hata", message: "Hatalı giriş")
+                    return
+                } else {
+                    self.createAnimatedPopUp(title: "Başarılı", message: "Öğrenci önerilmiştir.")
+                }
+            }
         }
+        
     }
-    func createAnimatedPopUp(title: String, message: String){
+    func createAnimatedPopUp(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "Tamam",style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
         self.present(alert, animated: true, completion: nil)
