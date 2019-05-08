6a3cd623080d434c9e26c0e7868c199474e8de43
NusretOzates <nozatespc@gmail.com>
2019-05-08 13:21:24 +0300

    Multithread support for the money transfer page

--- Tau-Pay/ParaGonder.swift
+++ Tau-Pay/ParaGonder.swift
@@ -59,74 +59,94 @@ class ParaGonder: UIViewController {
             return
         }
         
-        let money : Int? = Int(moneyAmount.text!)
-        
-        /*
-        if money! < 0 {
-            createAnimatedPopUp(title: "Çok zekisin ", message: "Para çalmaya çalışma artık Alp bitte!")
-            return
-        }
-        */
-        let json = [
-            "id":studentNumber.text!
-        ]
-        
-        let getname = Constants.SendRequestGetString(requestType: "/customers/get-name", json: json)
-        
-        if getname.connectionError {
-            createAnimatedPopUp(title: "Hata", message: "Bağlantı hatası, internete bağlantınızı kontrol ediniz ve birazdan tekrar deneyiniz")
-        }
-        else if getname.error != nil {
-            createAnimatedPopUp(title: "Hata", message: "Hata: \(getname.error!) tekrar deneyiniz")
-        }
-        else if getname.info == "user not found" {
-            createAnimatedPopUp(title: "Hata", message: "Hatalı Öğrenci Numarası")
-        }
-        else {
-            let name = getname.info
-            let alert = UIAlertController(title: "Emin misin", message: "\(name ?? "Bulunamadı") adli ogrenciye \(moneyAmount.text!) TL para gonderilecek emin misiniz ?", preferredStyle: UIAlertController.Style.alert)
+        let queue = DispatchQueue(label: "request")
+        queue.async {
+            let money : Int? = Int(self.moneyAmount.text!)
             
-            alert.addAction(UIAlertAction(title: "Eminim", style: UIAlertAction.Style.default, handler: {(action) in
-                self.sendMoneyRequest()
-            }))
+            /*
+             if money! < 0 {
+             createAnimatedPopUp(title: "Çok zekisin ", message: "Para çalmaya çalışma artık Alp bitte!")
+             return
+             }
+             */
+            let json = [
+                "id":self.studentNumber.text!
+            ]
             
-            alert.addAction(UIAlertAction(title: "Iptal", style: UIAlertAction.Style.default, handler: {(action) in
-                alert.dismiss(animated: true, completion: nil)
-            }))
+            let getname = Constants.SendRequestGetString(requestType: "/customers/get-name", json: json)
             
-            self.present(alert, animated: true, completion: nil)
+            DispatchQueue.main.async {
+                if getname.connectionError {
+                    self.createAnimatedPopUp(title: "Hata", message: "Bağlantı hatası, internete bağlantınızı kontrol ediniz ve birazdan tekrar deneyiniz")
+                }
+                else if getname.error != nil {
+                    self.createAnimatedPopUp(title: "Hata", message: "Hata: \(getname.error!) tekrar deneyiniz")
+                }
+                else if getname.info == "user not found" {
+                    self.createAnimatedPopUp(title: "Hata", message: "Hatalı Öğrenci Numarası")
+                }
+                else {
+                    let name = getname.info
+                    let alert = UIAlertController(title: "Emin misin", message: "\(name ?? "Bulunamadı") adli ogrenciye \(self.moneyAmount.text!) TL para gonderilecek emin misiniz ?", preferredStyle: UIAlertController.Style.alert)
+                    
+                    alert.addAction(UIAlertAction(title: "Eminim", style: UIAlertAction.Style.default, handler: {(action) in
+                        self.sendMoneyRequest()
+                    }))
+                    
+                    alert.addAction(UIAlertAction(title: "Iptal", style: UIAlertAction.Style.default, handler: {(action) in
+                        alert.dismiss(animated: true, completion: nil)
+                    }))
+                    
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+            
         }
+        
         
     }
     
-    func sendMoneyRequest()
-    {
-        
-        let json = [
-            "receiverId":studentNumber.text!,
-            "balanceId":selectedValue.lowercased(),
-            "amount": Double(moneyAmount.text!)!
-            ] as [String : Any]
-        
-        let response = Constants.SendRequestGetString(requestType: "/customers/transfer", json: json)
-        
-        if response.connectionError {
-            // Handle connection error
-            createAnimatedPopUp(title: "Hata", message: "Bağlantı hatası, internete bağlantınızı kontrol ediniz ve birazdan tekrar deneyiniz")
-            return
+    func sendMoneyRequest() {
+        let queue = DispatchQueue(label: "request")
+        queue.async {
+            let json = [
+                "receiverId":self.studentNumber.text!,
+                "balanceId":self.selectedValue.lowercased(),
+                "amount": Double(self.moneyAmount.text!)!
+                ] as [String : Any]
+            
+            
+                let response = Constants.SendRequestGetString(requestType: "/customers/transfer", json: json)
+                DispatchQueue.main.sync {
+                if response.connectionError {
+                    // Handle connection error
+                    self.createAnimatedPopUp(title: "Hata", message: "Bağlantı hatası, internete bağlantınızı kontrol ediniz ve birazdan tekrar deneyiniz")
+                }
+                else if response.error != nil {
+                    // Handle improper connection
+                    self.createAnimatedPopUp(title: "Hata", message: "Hatalı giriş")
+                }
+                else if response.info == "user not found" {
+                    self.createAnimatedPopUp(title: "Hata", message: "Hatalı Öğrenci Numarası")
+                }
+                else if response.info == "balance not found" {
+                    self.createAnimatedPopUp(title: "Hata", message: "bakiye bulunamadı")
+                }
+                else if response.info == "insufficient balance" {
+                    self.createAnimatedPopUp(title: "Sonuç", message: "Yetersiz bakiye")
+                }
+                else if response.info == "transfered successfully" {
+                    self.createAnimatedPopUp(title: "Sonuç", message: "Para başarıyla gönderildi.")
+                }
+                else {
+                    print(response.info!)
+                    print("Ooops Something went wrong")
+                }
+                
+            }
+            
+            Constants.getInfo()
         }
-        if response.error != nil {
-            // Handle improper connection
-            createAnimatedPopUp(title: "Hata", message: "Hatalı giriş")
-            return
-        }
         
-        if response.info == "balance not found" {
-            createAnimatedPopUp(title: "Hata", message: "Hatalı Öğrenci Numarası")
-            return
-        }
-        
-        createAnimatedPopUp(title: "Sonuç", message: "Para başarıyla gönderildi.")
-        Constants.getInfo()
     }
 }
