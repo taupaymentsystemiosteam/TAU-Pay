3c68671ec48ad0197553fb1fda3bcc83980ed806
NusretOzates <nozatespc@gmail.com>
2019-05-15 12:22:25 +0300

    m,,mn

--- Tau-Pay/Bagis.swift
+++ Tau-Pay/Bagis.swift
@@ -64,6 +64,7 @@ class FirstViewController: UIViewController {
             }
             }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
